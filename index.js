e53a0c2f8c853ed9d8c858d53f6a1e357595aa94
Charly Degut <charly@MacBook-Pro-de-Charly.local>
2026-05-04 18:02:50 +0200

    fix template id type

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post("/api/create-contract", async (req, res) => {
     const response = await axios.post(
       `${DOCUSEAL_BASE_URL}/api/submissions`,
       {
-        template_id: Number(templateId),
+        template_id: template_id,
         send_email: false,
         submitters: [
           {
